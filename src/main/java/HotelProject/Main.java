81ac8c0a879b70a73df220f34377e84591bc4a77
Jackson Belizário <jacksonluizzb@gmail.com>
2019-10-02 00:51:29 -0400

    Persistence

--- src/main/java/HotelProject/Main.java
+++ src/main/java/HotelProject/Main.java
@@ -5,6 +5,12 @@
  */
 package HotelProject;
 
+import HotelProject.persistence.entities.Funcionario;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author Jackson
@@ -16,6 +22,26 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Creating entity information...");
+        EntityManagerFactory entitiManagerFactory = Persistence.createEntityManagerFactory("hotel");
+        EntityManager entityManager = entitiManagerFactory.createEntityManager();
+
+        EntityTransaction et = entityManager.getTransaction();
+        
+        et.begin();
+        
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Jackson");
+        funcionario.setEndereco("Rua André Gomes Brandão");
+ 
+        entityManager.persist(funcionario);
+
+        et.commit();
+        
+        entityManager.close();
+ 
+        entitiManagerFactory.close();
+
     }
     
 }
---
+++ src/main/java/HotelProject/persistence/entities/Funcionario.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HotelProject.persistence.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Jackson
+ */
+
+@Entity
+@Table(name = "funcionario")
+public class Funcionario implements Serializable {
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "codigo", unique=true, nullable = false)
+    private String userId;
+    
+    @Column(name = "nome", nullable = false, length = 255)
+    private String nome;
+    
+    @Column(name = "endereco", nullable = true, length = 255)
+    private String endereco;
+    
+    @Column(name = "cidade", nullable = true, length = 255)
+    private String cidade;
+    
+    @Column(name = "estado", nullable = true, length = 255)
+    private String estado;
+    
+    @Column(name = "telefone", nullable = true, length = 255)
+    private String telefone;
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
